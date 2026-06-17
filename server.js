374600c2b37f50d135a7b1a1c6831a9c824a9aa3
Phi Yen <hnpyen05.work@gmail.com>
2026-06-17 22:26:02 +0700

    Khoi tao lai  BACKEND 1

---
+++ server.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+const app = require("./app");
+const config = require("./app/config")
+
+
+const PORT = config.app.port;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
